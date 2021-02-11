0aceddfb8183f0fc3905669832397e22cfc714dc
razvan-cretu <razvan.cretu@dynata.com>
2021-02-11 17:32:49 +0200

    rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DecipherAPIClient-razvan-cretu", # Replace with your own username
+    name="DecipherAPIClient", # Replace with your own username
     version="0.0.1",
     author="Razvan Cretu",
     author_email="razvan.cretu@dynata.com",
